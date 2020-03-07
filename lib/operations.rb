874cd1b8d64629d8d633e0e46fcd2fbe18833569
Dwight Nodolf <dnodolf@gmail.com>
2020-03-07 03:49:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,12 @@
 def unsafe?(speed)
+  if speed >60 || speed <40
+    return true
+    
+    
+  end
+    
+
+
 
 end
 
@@ -10,3 +18,13 @@ end
 	
 
 
+# inputs --> speed below 40 greater than 60 numbers/integers
+
+#outputs --> true and false answers /booleans
+
+#speeds below 40 are unsafe
+#speeds above 60 are unsafe
+#if we input unsafe integers the method will result in a true return
+#else we get safe integers the method will result in a false return
+#If your speed is >60 OR <40 say true
+#otherwise, say false
